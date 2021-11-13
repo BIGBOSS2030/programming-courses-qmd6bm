15fd907dacd144f65bb73b6b81e24320198c62a4
BIGBOSS2030 <elkhiaririad466@gmail.com>
2021-11-13 19:00:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-qmd6bm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-qmd6bm)
---
+++ index.ts
@@ -0,0 +1,34 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let riad1: number;
+let riad2: number;
+
+console.log(riad1+riad2)
+
+
+let bigboss1:number;
+let bigboss2:number;
+let bigboss3:number;
+let bigboss4:number;
+
+console.log(bigboss1+bigboss2+bigboss3+bigboss4)
+
+let khadija:number;
+let khadija2:number;
+
+console.log(khadija-khadija2)
+  
+let abdo:number;
+let abdo2:number;
+
+console.log(abdo*abdo2)
+
+let badr1:number;
+let badr2:number;
+let reda1:number;
+let reda2:number;
+
+console.log(badr1/badr2/reda1/reda2)
+  
